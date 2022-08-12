42b376a49ee7e883c9f709aa3998b64ff5562b65
Abigail Ross <aross@aforza.com>
2022-08-12 16:26:00 +0100

    Started implementation of track preview play

--- src/ui/components/tracks/TrackPreview.tsx
+++ src/ui/components/tracks/TrackPreview.tsx
@@ -1,3 +1,4 @@
+import ReactPlayer from "react-player";
 import { Track } from "../../../types/track";
 import styles from "./Tracks.module.css";
 
@@ -5,15 +6,50 @@ type Props = {
   track: Track;
 };
 
+const size = 180;
+
 const TrackPreview = (props: Props) => {
   const { track } = props;
 
-  const { name, explicit, artists, popularity } = track;
+  const {
+    name,
+    explicit,
+    artists,
+    popularity,
+    preview_url,
+    album,
+    is_playable,
+  } = track;
+
+  const pp = album.images[0];
 
   const artistsFormatted = artists.map((artist) => artist.name).join(", ");
 
   return (
     <div className={styles["TrackPreview"]}>
+      {pp.url && is_playable ? (
+        <ReactPlayer
+          light={pp.url || true}
+          playing={false}
+          muted={false}
+          url={preview_url}
+          width={180}
+          height={180}
+        />
+      ) : (
+        <img
+          src={pp.url}
+          style={{
+            width: size,
+            height: size,
+            objectFit: "cover",
+            objectPosition: "100% 0%",
+            aspectRatio: "auto",
+            borderRadius: 100,
+          }}
+          alt="Album"
+        />
+      )}
       <div className={styles["TrackNameWrapper"]}>
         <p className={styles["TrackName"]}>{name}</p>
       </div>
