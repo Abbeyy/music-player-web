f600d8e2625a23f7709f61a705e0f4f32b00bd54
Abigail Ross <aross@aforza.com>
2022-08-26 22:33:51 +0100

    Implemented show More and Less conditionally on the artists profile

--- src/ui/components/artists/profile/ArtistAlbums.tsx
+++ src/ui/components/artists/profile/ArtistAlbums.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Album } from "../../../../types/album";
 import { AlbumPreview } from "../../albums/AlbumPreview";
 import styles from "./ArtistAlbums.module.css";
@@ -6,23 +7,53 @@ type Props = {
   albums: { [key: string]: Album[] };
 };
 
+const LIMIT = 4;
+
 export const ArtistsAlbums = (props: Props) => {
   const { albums } = props;
 
+  const [showMore, setShowMore] = useState(() =>
+    Object.keys(albums).reduce(
+      (acc: { [key: string]: boolean }, type: string) => {
+        acc[type] = false;
+        return acc;
+      },
+      {}
+    )
+  );
+
   const content: JSX.Element[] = [];
   Object.entries(albums).forEach((entry) => {
-    const [type, albums] = entry;
+    const [type, albumsForType] = entry;
+    const albumsAfterLimit = showMore[type]
+      ? albumsForType
+      : albumsForType.slice(0, LIMIT);
+
     content.push(
       <>
         <h3 className={styles["Info"]}>{`${type}S`}</h3>
+        {albumsForType.length > LIMIT ? (
+          <button
+            className={styles["ShowHide"]}
+            onClick={() => handleShowHide(type)}
+          >
+            {showMore[type] ? `Show Less ${type}s` : `Show More ${type}s`}
+          </button>
+        ) : null}
         <div className={styles["AlbumPanel"]}>
-          {albums?.map((album) => (
-            <AlbumPreview album={album as Album} />
+          {albumsAfterLimit?.map((currentAlbum) => (
+            <AlbumPreview album={currentAlbum as Album} />
           ))}
         </div>
       </>
     );
   });
+
+  const handleShowHide = (albumType: string) =>
+    setShowMore((prevState) => {
+      const previousValue = prevState[albumType];
+      return { ...prevState, [albumType]: !previousValue };
+    });
 
   return (
     <div className={styles["Panel"]}>
